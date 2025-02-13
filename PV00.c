04c5735b59af7d0198718b9615a078ae700d1740
Maksym Sushko <maxmashkovmax@gmail.com>
2025-02-13 16:56:16 +0200

    first correct version of PV00

--- PV00.c
+++ PV00.c
@@ -4,32 +4,30 @@
 typedef unsigned char COE; // COE <-> Code Of End
 typedef unsigned char BYTE;
 
-#define DIV 0x00 // data is void
+#define DIV 0x00 // data is void for write
 #define AG 0x01 // always good
 #define FINO 0x02 // file is not open
 #define UCW 0x03 // uncorrect write
+#define FATAL_ERROR 0x04 // fatal error
 
 typedef struct DATA_ {
 	int length;
 	BYTE* Data;
 } DATA;
 
-// BYTE IntegerLog2(int num) {
-// 	BYTE result = 0;
-// 	while (num != 0) {
-// 		result++;
-// 		num >>= 2;
-// 	}
-// 	return result;
-// }
+COE CreateVoidData(int length, DATA* data) {
+	data->length = length;
+	data->Data = (BYTE*)malloc(length);
+	if (data->Data == NULL || data->length == 0) return DIV;
+	return AG;
+}
 
-COE WriteData(DATA path, DATA* data, BYTE block) {
-	if (data->length == 0) return DIV;
-	FILE* file = fopen(path.Data, "ab");
+COE WriteData(char* path, DATA* data, BYTE block) {
+	FILE* file = fopen(path, "ab");
 	if (file == NULL) return FINO;
 
 	BYTE head = 0; // head = (lenght of Data) + block(number) 
-	head += (block << 6) >> 6;
+	head += block & 0b00000011;
 	head += (data->length - 1) << 2; // 0 < LengthData <= 64
 
 	int SumLength = 1 + data->length;
@@ -43,118 +41,81 @@ COE WriteData(DATA path, DATA* data, BYTE block) {
 	return AG;
 }
 
-COE ReadSectionData(char* path, DATA* data, BYTE block, int StartPoSym, int LengthSectionData) { //PoSym <-> position symbol
+COE ReadSectionData(char* path, DATA* data, BYTE block, int FirstImos, int LengthSectionData) { //PoSym <-> position symbol
 	FILE *file = fopen(path, "rb");
-	if (file == NULL) {
-		return FINO;
-	}
+	if (file == NULL) return FINO;
 
 	BYTE Head = 1;
-	BYTE LengthData, BlockNum;
+	BYTE LengthDataBlock, BlockNum;
 
 	fseek(file, 0, SEEK_SET);
-	int current_seek_in_block = 0;
+	int SEEK_DATA = 0;
+	int SEEK_SECTION_DATA = 0;
 
 	int StartToPoint, EndToPoint;
 
-	int time = 0;
-
-	while (Head != 0) {
-		time++;
-		if (time > 6) {
-			return DIV;
-		}
+	// find start needs section in data
+	while (1) {
 		//upload info of head
 		fread(&Head, 1, 1, file);
-		LengthData = (Head >> 2) + 1;
+		LengthDataBlock = (Head >> 2) + 1;
 		BlockNum = Head & 0b00000011;
-		printf("first block\nLengthData: %hhu\nBlockNum: %hhu\n\n", LengthData, BlockNum);
-		//upload info of head
 
 		if (BlockNum == block) {
 
-			StartToPoint = (StartPoSym) - current_seek_in_block;
+			StartToPoint = FirstImos - SEEK_DATA;
 			EndToPoint = StartToPoint + LengthSectionData;
 
-			if (StartToPoint > LengthData) {
-				printf("0");
-				current_seek_in_block += LengthData;
-				fseek(file, LengthData + 1, SEEK_CUR);
-			}
-			else if (EndToPoint <= LengthData) {
-				printf("1");
-				fseek(file, (int)StartToPoint, SEEK_CUR);
-				printf(" %hhu", StartToPoint);
-				fread(data->Data, 1, (size_t)LengthSectionData, file); //
-				fclose(file);
-				return AG;
+			// LOG: printf("first block\nLengthData: %hhu\nBlockNum: %hhu\nStartToPoint: %i\nEndToPoint: %i\n\n", LengthDataBlock, BlockNum, StartToPoint, EndToPoint);
+
+			if (StartToPoint < LengthDataBlock) {
+				fseek(file, StartToPoint, SEEK_CUR);
+				if (EndToPoint < LengthDataBlock) {
+					fread(data->Data, 1, EndToPoint - StartToPoint, file);
+					SEEK_DATA += EndToPoint - StartToPoint;
+					return AG;
+				}
+				else {
+					fread(data->Data, 1, LengthDataBlock - StartToPoint, file);
+					SEEK_DATA += LengthDataBlock;
+					SEEK_SECTION_DATA += LengthDataBlock - StartToPoint;
+					break;
+				}
 			}
 			else {
-				printf("2");
-				fseek(file, StartToPoint, SEEK_CUR);
-				fread(data->Data, 1, LengthData - StartToPoint, file); //
-				break;
+				fseek(file, LengthDataBlock, SEEK_CUR);
+				SEEK_DATA += LengthDataBlock;
 			}
 		}
-		else {
-			fseek(file, LengthData + 1, SEEK_CUR);
-		}
+		else fseek(file, LengthDataBlock, SEEK_CUR);
 	}
 
-
-	while (Head != 0) {
+	// write data to section data end find end section data in data 
+	while (1) {
 		//upload info of head
 		fread(&Head, 1, 1, file);
-		LengthData = (Head >> 2) + 1;
+		LengthDataBlock = (Head >> 2) + 1;
 		BlockNum = Head & 0b00000011;
-		//upload info of head
-		printf("second block\nLengthData: %hhu\nBlockNum: %hhu\n\n", LengthData, BlockNum);
 
 		if (BlockNum == block) {
 
-			StartToPoint = (StartPoSym) - current_seek_in_block + 1; //
-			EndToPoint = StartToPoint + LengthSectionData;			 // Time-code
+			StartToPoint = FirstImos - SEEK_DATA;
+			EndToPoint = StartToPoint + LengthSectionData;
 
-			if (EndToPoint < LengthData) {
-				fread(data->Data + current_seek_in_block, 1, EndToPoint, file); //
-				fclose(file);
+			// LOG: printf("second block\nLengthData: %hhu\nBlockNum: %hhu\nStartToPoint: %i\nEndToPoint: %i\nSEEK_SECTION_DATA: %i\n\n", LengthDataBlock, BlockNum, StartToPoint, EndToPoint, SEEK_SECTION_DATA);
+
+			if (EndToPoint < LengthDataBlock) {
+				fread(data->Data + SEEK_SECTION_DATA, 1, EndToPoint, file);
 				return AG;
 			}
 			else {
-				fread(data->Data + current_seek_in_block, 1, LengthData, file); //
+				fread(data->Data + SEEK_SECTION_DATA, 1, LengthDataBlock, file);
+				SEEK_DATA += LengthDataBlock;
+				SEEK_SECTION_DATA += LengthDataBlock;
 			}
 		}
-		else {
-			fseek(file, LengthData + 1, SEEK_CUR);
-		}
+		else fseek(file, LengthDataBlock, SEEK_CUR);
 	}
-	return DIV;
-}
 
-int main() { // testing protocole pv00(It is time-code)
-	DATA path = {0, "tet.bin"};
-	DATA data = {6, "hibro"};
-
-	DATA testdata = {2, "pp"};
-	//WriteData(path, &data, 3);
-
-	COE sec = ReadSectionData("tet.bin", &testdata, 3, 3, 2);
-	if (sec == AG) {
-		printf("it is fully pizda");
-	} else {
-		if (sec == DIV) {
-			printf("diva");
-		}
-		else {
-			printf("%i", sec);
-		}
-		if (sec == FINO) {
-			printf("lll");
-		}
-	}
-	printf("\n%c%c", *testdata.Data, *(testdata.Data + 1));
-	int i;
-		while(1) {
-			i++;
-		}
+	return FATAL_ERROR;
 }
